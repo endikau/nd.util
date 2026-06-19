58ef2de399c52c3fc4fce2b62c01c6bd4fed980c
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2026-06-19 23:01:19 +0200

    update

--- R/render_raw_html.R
+++ R/render_raw_html.R
@@ -7,13 +7,19 @@
 #'
 #' @examples
 #' NULL
-render_raw_html <- function(.html){
-  .html |> 
-    purrr::map_chr(as.character) |> 
-    stringi::stri_c(collapse="\n") |> 
-    (\(.str){stringi::stri_c(
-      "{{{< raw_html >}}}", "```{=html}", .str, "{{{< /raw_html >}}}", "```",
-      sep="\n"
-    )})() |> 
+render_raw_html <- function(.html) {
+  .html |>
+    purrr::map_chr(as.character) |>
+    stringi::stri_c(collapse = "\n") |>
+    (\(.str) {
+      stringi::stri_c(
+        "{{{< raw_html >}}}",
+        "```{=html}",
+        .str,
+        "```",
+        "{{{< /raw_html >}}}",
+        sep = "\n"
+      )
+    })() |>
     knitr::asis_output()
 }
